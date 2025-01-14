6812a67a4ff5759a0b03ad603d44d6810fb702a0
Michael-zionist <michael.svoboda2@mail.dcu.ie>
2025-01-14 14:01:37 +0000

    Motor Test Upload
    Uploaded the DCU-provided Motor Test

---
+++ Motor-test/Motor-test.ino
@@ -0,0 +1,21 @@
+int motor1PWM = 37;
+int motor1Phase = 38;
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+
+  // Configure pins as outputs
+  pinMode(motor1PWM, OUTPUT);
+  pinMode(motor1Phase, OUTPUT);
+}
+// the loop routine runs over and over again continuously:
+void loop() {
+  digitalWrite(motor1Phase, HIGH); //forward
+  analogWrite(motor1PWM, 100); // set speed of motor
+  Serial.println("Forward"); // Display motor direction
+  delay(2000); //2 seconds
+  digitalWrite(motor1Phase, LOW); //Backward
+  analogWrite(motor1PWM, 100); // set speed of motor
+  Serial.println("Backward"); // Display motor direction
+  delay(2000); //2 seconds
+}
