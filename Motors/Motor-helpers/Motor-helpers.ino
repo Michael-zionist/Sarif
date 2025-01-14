86b08846203fb0b2d6ae1b811444e069559700c3
Michael-zionist <michael.svoboda2@mail.dcu.ie>
2025-01-14 15:57:54 +0000

    Cleaning up data structure

--- Motors/Motor-helpers/Motor-helpers.ino
+++ Motors/Motor-helpers/Motor-helpers.ino
@@ -3,6 +3,16 @@ int m1PhasePin = 38;
 int m2PWMPin = 39;
 int m2PhasePin = 20;
 
+// blinks onboard LED x times with a 0.6s total on-off period
+void blinkLED(int times) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on
+    delay(300);                      // Wait for 0.3 seconds
+    digitalWrite(LED_BUILTIN, LOW);  // Turn the LED off
+    delay(300);                      // Wait for 0.3 seconds
+  }
+}
+
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(9600);
