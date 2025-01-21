c4e46c6d1b010ad1f6b14fce2aeea646fad9f59d
Cora454 <corayvonne@gmail.com>
2025-01-21 11:11:48 +0000

    Update arrray-to-decimal.py

--- Array-to-decimal /arrray-to-decimal.py
+++ Array-to-decimal /arrray-to-decimal.py
@@ -16,32 +16,8 @@ def arrayToDecimal(arr, threshold):
 
 
 
-#numbers = [0.5, 1.3, 0.7, 2.9, 3.2]   #array coming in 
+numbers = [0.5, 1.3, 0.7, 2.9, 3.2]   #sample array coming in 
 
-thresholdValue = 2.0     #threshold 
+thresholdValue = 2.0     #threshold value
 
-decimalResult = arrayToDecimal(AnalogValue, thresholdValue)  #how to run the function 
-
-#_____________________________________
-
-int AnalogValue[5] = {0,0,0,0,0}
-int AnalogPin[5] = {4,5,6,7,15} # keep 8 free for tone O/P music
-
-void setup()
-{
-    Serial.begin(9600);
-}
-
-void loop()
-{
-    int i;
-    for (i=0;i<5;i++) {
-        AnalogValue[i]=analogRead(AnalogPin[i])
-        Serial.print(AnalogValue[i]) #This prints the actual analog reading from the sensors
-        Serial.print("\t") #tab over on screen
-        if(i==4) {
-            Serial.println("") #carriage return
-            delay(600) # display new set of readings every 600mS
-        }
-    }
-}
+decimalResult = arrayToDecimal(numbers, thresholdValue)  #how to run the function 
