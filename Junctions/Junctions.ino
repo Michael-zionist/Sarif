3412a191879272419b17e95a5f502ff644f48020
Cora454 <corayvonne@gmail.com>
2025-01-28 15:20:35 +0000

    Update Junctions.ino

--- Junctions/Junctions.ino
+++ Junctions/Junctions.ino
@@ -14,7 +14,7 @@ int AnalogPin[5] = {A1pin, A2pin, A3pin, A4pin, A5pin};
 // ...motor calibration (native to script):
 const float slowingCoeff = 0.92;  // Makes more efficient L motor slower to match R
 const int topSpeed = 150;
-const int parkDistance = 2000;
+const int parkDistance = 2000; //distance at which is stops before wall 
 
 // Navigation array has set structure: [N, Nf, N, N, Nf, N, Np, N, I, I, I, B]
 // N - Navigation node (Nf - fictional, Np - parking)
