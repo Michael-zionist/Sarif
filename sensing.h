b3d9288e3a447cb1a221acf6c8da618c6de3dd06
Nathan <nathan.stringer2@mail.dcu.ie>
2025-02-03 21:07:50 +0000

    Began Populating sensing.h

---
+++ sensing.h
@@ -0,0 +1,26 @@
+/*****************************************************************************
+sensing.h - Contains "parking()", "readDistanceSensor()" and "readSensors()"
+*****************************************************************************/
+
+#ifndef SENSING_H
+#define SENSING_H
+
+class sensing{
+    public:
+        // Parking Function
+        void parking(int whiteThreshold = 2700){
+
+        }
+
+        // Distance Sensor Reading Function
+        int readDistanceSensor(){
+
+        }
+
+        // Read Sensors Function
+        int readSensors(int whiteThreshold, int* AnalogPin){
+
+        }
+};
+
+#endif // SENSING_H
