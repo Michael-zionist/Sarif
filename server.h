41f054bd44069271484055a11e131e75f5b3e969
Nathan <nathan.stringer2@mail.dcu.ie>
2025-02-04 11:16:36 +0000

    Revert "Began WiFi Classes Test"
    This reverts commit 6410085ba3dd6d49bbd43fc22852ed44c6ebd6df.

--- server.h
+++ server.h
@@ -9,7 +9,7 @@ server.h - Contains "wiFiConnect()" and "destinationFetch()"
 #ifndef SERVER_H
 #define SERVER_H
 
-class Server{
+class server{
     public:
         // WiFi Connection Function
         void wiFiConnect(){
